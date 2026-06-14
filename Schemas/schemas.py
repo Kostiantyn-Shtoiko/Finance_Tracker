f8458293c68947bec97dbd4d256ed566eee8ef3c
Kostya <shtoykokostya8@gmail.com>
2026-06-14 19:34:18 +0200

    Add Pydantic schemas for profile and transactions

---
+++ Schemas/schemas.py
@@ -0,0 +1,36 @@
+from pydantic import BaseModel, Field
+from typing import Literal
+
+
+class ProfileRegisterSchema(BaseModel):
+    last_name: str
+    first_name: str
+    phone: str 
+    password: str
+
+class ProfileSchema(BaseModel):
+    id: int         
+    last_name: str  
+    first_name: str 
+    phone: str      
+
+class ProfileLoginSchema(BaseModel):
+    phone: str 
+    password: str
+
+class TransactionAddSchema(BaseModel):
+    amount: float
+    category: str
+    title: str | None = Field(max_length=100)
+    type: Literal["income", "expense"]
+    date: str
+
+class TransactionSchema(BaseModel):
+    id: int
+    user_id: int
+    amount: float
+    category: str
+    title: str | None = Field(max_length=100)
+    type: Literal["income", "expense"]
+    date: str
+
