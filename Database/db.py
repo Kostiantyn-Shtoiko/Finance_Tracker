7fcb06c96841ec3cae107abca7390f2d18869db7
Kostya <shtoykokostya8@gmail.com>
2026-06-14 19:33:49 +0200

    Add database connection and session setup

--- Database/db.py
+++ Database/db.py
@@ -1,1 +1,13 @@
-from models.models import engine, Base 
+from Models.models import engine, Base 
+from sqlalchemy.ext.asyncio import AsyncSession
+
+new_session = AsyncSession(engine, expire_on_commit=False)
+
+async def get_session():
+    async with new_session as session:
+        yield session
+
+async def setup_database():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    return {"success": True, "message": "Database setup completed!"}
